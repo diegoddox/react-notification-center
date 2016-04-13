2ccca432cb0a6de222f9d252b69039766ff84bda
Diego Oliveira <diegoddox@gmail.com>
2016-04-13 08:23:46 +0200

    remove bootstrap from the project

--- build/webpack/base.js
+++ build/webpack/base.js
@@ -60,7 +60,6 @@ module.exports = {
             + '   <meta charset="iso-8859-1">'
             + '   <title>React Notification Center</title>'
             + '   <meta name="viewport" content="width=device-width, initial-scale=1">'
-            // + '   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">'
             + ' </head>'
             + ' <body>'
             + '   <div id="app"></div>'
